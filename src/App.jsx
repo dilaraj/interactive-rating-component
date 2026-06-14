8f7ff556d67404f55792883e75303d3f4bb1bf58
dilaraj <dilara.ajaj369@gmail.com>
2026-06-14 17:58:49 +1000

    Completing main rating component for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import './App.css'
+import starIcon from './assets/images/icon-star.svg'
 
 export default function App() {
   return (
     <div className="master-container">
-      Interactive rating component
+      <div className="star-container">
+        <img src={starIcon} alt="Star Image" />
+      </div>
+
+      <div className="header">How did we do?</div>
+      <div className="body-text">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</div>
+    
+      <div className="rating-container">
+        <div className="rating" data-rating='1'>1</div>
+        <div className="rating" data-rating='2'>2</div>
+        <div className="rating" data-rating='3'>3</div>
+        <div className="rating" data-rating='4'>4</div>
+        <div className="rating" data-rating='5'>5</div>
+      </div>
+
+      <button type="submit" className='submit-btn'>Submit</button>
+    
     </div>
   )
 }
